bb47c86b97ecadb84378954d0b4c1449a278466a
Alexander Parfenov <demoh748@gmail.com>
2015-02-12 22:51:38 +0600

    List implementation changed to two-linked list.

--- raster/list.c
+++ raster/list.c
@@ -4,10 +4,12 @@
 typedef struct TDYN_LIST_ELEMENT {
 	DynListElement data;
 	struct TDYN_LIST_ELEMENT* next;
+	struct TDYN_LIST_ELEMENT* prev;
 } DynListElementS;
 
 typedef struct TDYN_LIST {
 	DynListElementS* head;
+	DynListElementS* tail;
 	uint size;
 } DynListS;
 
@@ -15,6 +17,7 @@ DynList dynlist_init() {
 	DynListS* list = (DynListS*)malloc(sizeof(DynListS));
 	list->head = NULL;
 	list->size = 0;
+	list->tail = NULL;
 	return list;
 }
 
@@ -66,20 +69,21 @@ void dynlist_set_element(DynList list_descriptor, uint index, DynListElement new
 
 void dynlist_push_back(DynList list_descriptor, DynListElement new_element) {
 	DynListS* list = (DynListS*)list_descriptor;
-	DynListElementS* element = list->head;
+	DynListElementS* element = list->tail;
 
 	DynListElementS* new_elementS = (DynListElementS*)malloc(sizeof(DynListElementS));
 	new_elementS->data = new_element;
 	new_elementS->next = NULL;
+	new_elementS->prev = NULL;
 
 	if (element) {
-		while (element->next) {
-			element = element->next;
-		}
 		element->next = new_elementS;
+		new_elementS->prev = element;
 	} else {
+		list->tail = new_elementS;
 		list->head = new_elementS;
 	}
+	list->tail = new_elementS;
 	list->size++;
 }
 
@@ -88,16 +92,22 @@ void dynlist_remove(DynList list_descriptor, uint index) {
 	if (index >= list->size)
 		return; // TODO: Write error
 	DynListElementS* for_delete = NULL;
-	if (index != 0) {
+	if (index == 0) { // Delete head
+		for_delete = list->head;
+		list->head = for_delete->next;
+		list->head->prev = NULL;
+	} else if (index == list->size - 1) {
+		for_delete = list->tail;
+		list->tail = for_delete->prev;
+		list->tail->next = NULL;
+	} else {
 		DynListElementS* prev_element = list->head;
 		for (uint i = 0; i < index - 1; ++i) {
 			prev_element = prev_element->next;
 		}
 		for_delete = prev_element->next;
-		prev_element->next = for_delete->next;
-	} else { // Delete head
-		for_delete = list->head;
-		list->head = for_delete->next;
+		for_delete->prev->next = for_delete->next;
+		for_delete->next->prev = for_delete->prev;
 	}
 	free(for_delete->data);
 	free(for_delete);
